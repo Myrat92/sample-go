6cd5c7865ef6ed9b46d3c3982b23b7c04990021d
Myrat92 <zjsegzhf@gmail.com>
2022-03-01 16:47:50 +0800

    upload testserver

---
+++ test-server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "hello")
+	})
+
+	r.GET("/fib", func(c *gin.Context) {
+		num := c.Query("num")
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			c.String(http.StatusBadRequest, "Request not a number format")
+		}
+
+		res := fib(n)
+		c.String(http.StatusOK, "Fibonacci is %s", strconv.Itoa(res))
+	})
+}
+
+func fib(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	return fib(n-2) + fib(n-1)
+}
